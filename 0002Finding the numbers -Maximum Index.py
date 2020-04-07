53d575a15370dfd96c43458e5f28a729a8bc6237
apoorvamalemath <35571354+apoorvamalemath@users.noreply.github.com>
2020-04-07 15:58:11 +0530

    Create 0002Finding the numbers -Maximum Index.py

---
+++ 0002Finding the numbers -Maximum Index.py
@@ -0,0 +1,44 @@
+"""
+You are given an array A containing 2*N+2 positive numbers, out of which 2*N numbers exist in pairs whereas the other two number occur exactly once and are distinct. You need to find the other two numbers and print them in ascending order.
+ 
+
+Input :
+The first line contains a value T, which denotes the number of test cases. Then T test cases follow .The first line of each test case contains a value N. The next line contains 2*N+2 space separated integers.
+
+Output :
+Print in a new line the two numbers in ascending order.
+
+Constraints :
+1<=T<=100
+1<=N<=10^6
+1<=A[i]<=5*10^8
+
+Example:
+Input :
+2
+2
+1 2 3 2 1 4
+1
+2 1 3 2
+
+Output :
+3 4
+1 3
+"""
+
+def Find( ele ):
+    index=dict()
+    for i in range(len(ele)):
+        if(ele[i] in index):
+            index.pop(ele[i])
+        else:
+            index[ele[i]] =[i]
+    indexL=(sorted(index))
+    print(' '.join(map(str, indexL)))
+
+test_cases=input()
+for i in range(int(test_cases)):
+    no_ele=input()
+    ele =list()
+    ele = list(map(int, input().split()))
+    Find(ele)
